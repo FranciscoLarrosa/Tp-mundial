24abd5948df879b5c0f7c801541afdd565c92163
Francisco Larrosa <francisco_larrosa_96@hotmail.com>
2018-04-17 23:59:38 -0300

    TP

---
+++ MundialDeFutbol/src/mundiadefutbol/Partido.java
@@ -0,0 +1,38 @@
+package mundiadefutbol;
+
+
+
+public class Partido {
+    //Variables:
+   
+    private Equipo local;
+    private Equipo visitante;
+    private Resultado resultado = new Resultado();
+    //Constructor : 
+    public Partido( Equipo local, Equipo visitante) {
+        
+        this.visitante = visitante;
+        this.local = local;
+
+    }
+
+    public void setLocal(Equipo local) {
+        this.local = local;
+    }
+
+    public void setVisitante(Equipo visitante) {
+        this.visitante = visitante;
+    }
+    //Devuelve un resultado
+    public Resultado getResultado(){
+        return resultado;
+    }
+    //Retorna el equipo local
+    public Equipo getLocal(){
+        return local;
+    }
+    //Retorna el equipo Visitante
+    public Equipo getVisitante(){
+        return visitante;
+    }
+}
